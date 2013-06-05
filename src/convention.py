02608707e6a8d9d6123b24308312d3473cf5ec1d
renzo <renzo.n@gmail.com>
2013-06-05 14:59:47 -0300

    Basic Hello World working with template

--- src/convention.py
+++ src/convention.py
@@ -2,23 +2,26 @@
 from __future__ import absolute_import, unicode_literals
 import sys
 import os
-#Put lib on path, once Google App Engine does not allow doing it directly
-sys.path.append(os.path.join(os.path.dirname(__file__),"lib"))
 
+#Put lib on path, once Google App Engine does not allow doing it directly
+sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+
+import tmpl
 import logging
 import traceback
 import webapp2
 from zen import router
 from zen.router import PathNotFound
 
-def _extract_values(handler,param,default_value=""):
-    values=handler.request.get_all(param)
+
+def _extract_values(handler, param, default_value=""):
+    values = handler.request.get_all(param)
     if param.endswith("[]"):
-        return param[:-2],values if values else []
+        return param[:-2], values if values else []
     else:
-        if not values: return param,default_value
-        if len(values)==1: return param,values[0]
-        return param,values
+        if not values: return param, default_value
+        if len(values) == 1: return param, values[0]
+        return param, values
 
 
 class BaseHandler(webapp2.RequestHandler):
@@ -29,18 +32,24 @@ class BaseHandler(webapp2.RequestHandler):
         self.make_convetion()
 
     def make_convetion(self):
-        kwargs=dict(_extract_values(self,a) for a in self.request.arguments())
-        convention_params={"_req": self.request,"_resp": self.response,
-                           "_handler": self}
+        kwargs = dict(_extract_values(self, a) for a in self.request.arguments())
+        def write_tmpl(template_name,values={}):
+            return self.response.write(tmpl.render(template_name,values))
+        convention_params = {"_req": self.request,
+                             "_resp": self.response,
+                             "_handler": self,
+                             "_render": tmpl.render,
+                             "_write_tmpl":write_tmpl}
+        convention_params["_dependecies"] = convention_params
         try:
-            fcn,params = router.to_handler(self.request.path,convention_params,**kwargs)
-            fcn(*params,**kwargs)
+            fcn, params = router.to_handler(self.request.path, convention_params, **kwargs)
+            fcn(*params, **kwargs)
         except PathNotFound:
-            logging.error("Path not Found: "+self.request.path)
+            logging.error("Path not Found: " + self.request.path)
         except:
-            logging.error((fcn,params,kwargs))
+            logging.error((fcn, params, kwargs))
             logging.error(traceback.format_exc())
 
 
-app = webapp2.WSGIApplication([("/.*", BaseHandler)], debug = False)
+app = webapp2.WSGIApplication([("/.*", BaseHandler)], debug=False)
 
