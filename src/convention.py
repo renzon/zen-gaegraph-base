8c4e4d8a00e9f5bcdde9a43b075dc011c3d100f8
renzo <renzo.n@gmail.com>
2013-06-05 14:25:44 -0300

    Basic Hello World working without template

---
+++ src/convention.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import sys
+import os
+#Put lib on path, once Google App Engine does not allow doing it directly
+sys.path.append(os.path.join(os.path.dirname(__file__),"lib"))
+
+import logging
+import traceback
+import webapp2
+from zen import router
+from zen.router import PathNotFound
+
+def _extract_values(handler,param,default_value=""):
+    values=handler.request.get_all(param)
+    if param.endswith("[]"):
+        return param[:-2],values if values else []
+    else:
+        if not values: return param,default_value
+        if len(values)==1: return param,values[0]
+        return param,values
+
+
+class BaseHandler(webapp2.RequestHandler):
+    def get(self):
+        self.make_convetion()
+
+    def post(self):
+        self.make_convetion()
+
+    def make_convetion(self):
+        kwargs=dict(_extract_values(self,a) for a in self.request.arguments())
+        convention_params={"_req": self.request,"_resp": self.response,
+                           "_handler": self}
+        try:
+            fcn,params = router.to_handler(self.request.path,convention_params,**kwargs)
+            fcn(*params,**kwargs)
+        except PathNotFound:
+            logging.error("Path not Found: "+self.request.path)
+        except:
+            logging.error((fcn,params,kwargs))
+            logging.error(traceback.format_exc())
+
+
+app = webapp2.WSGIApplication([("/.*", BaseHandler)], debug = False)
+
---
+++ src/web/home.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+def index(_resp):
+    _resp.write("Hello World")
